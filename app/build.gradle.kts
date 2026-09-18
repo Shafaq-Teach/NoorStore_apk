a714b936317cdc9ab3915027ca18e19d30895675
SerSan <yulgun353@gmail.com>
2026-09-18 19:51:36 +0300

    Test build for v1.0.2 with single icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.aistudio.noorstore.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 2
+        versionName = "1.0.2"
     }
 
     buildTypes {
